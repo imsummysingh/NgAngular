772194c54c635ff02f0fedff3cd097f010e98390
Summy Singh <Summy.Singh@cbre.com>
2022-01-19 16:26:40 +0530

    section done

--- ngAngularLearning/src/app/app.component.ts
+++ ngAngularLearning/src/app/app.component.ts
@@ -6,23 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  serverElements = [{type:'server',name:'TestServer',content:'Just a Test'}];
 
-  onAddServer() {
+  oddNumbers:number[]=[];
+  evenNumbers:number[]=[];
+  
+  onServerAdded(serverData:{serverName:string, serverContent:string}) {
     this.serverElements.push({
       type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: serverData.serverName,
+      content: serverData.serverContent
     });
   }
 
-  onAddBlueprint() {
+  onBlueprintAdded(blueprintData:{serverName:string, serverContent:string}) {
     this.serverElements.push({
       type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
     });
+  }
+
+  onIntervalFired(firedNumber:number){
+    if(firedNumber%2===0){
+      this.evenNumbers.push(firedNumber);
+    }else{
+      this.oddNumbers.push(firedNumber);
+    }
   }
 }
