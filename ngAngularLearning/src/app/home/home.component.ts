67356b6ef96e0563ffe7c6c9f1232f170cfbc195
Summy Singh <Summy.Singh@cbre.com>
2022-01-25 15:49:56 +0530

    Observable starter added

--- ngAngularLearning/src/app/home/home.component.ts
+++ ngAngularLearning/src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,26 +7,9 @@ import { AuthService } from '../auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  // onLoadServers(){
-  //   //complex calculation and some logic and now we want to navigate away after performing this operation
-  //   this.router.navigate(['/servers']);
-  // }
-
-  onLoadServer(id:number){
-    this.router.navigate(['/servers',id,'edit'], {queryParams: {allowEdit:'1'}, fragment:'loading'});
-  }
-
-  onLogin(){
-    this.authService.loggedIn=true;
-  }
-
-  onLogout(){
-    this.authService.loggedIn=false;
   }
 
 }
